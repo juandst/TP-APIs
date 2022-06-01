3655a76faf73c0393a22a6bb1eeb2d77193ff2cb
juan-ds <juanods1@gmail.com>
2022-06-01 16:12:18 -0300

    Remove unecessary creation of "Valoraciones" list

--- TP-APIs/Models/PeliculaUpdateDto.cs
+++ TP-APIs/Models/PeliculaUpdateDto.cs
@@ -1,13 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 namespace TP_APIs.Models
 {
-    public class PeliculaCrearDto
+    public class PeliculaUpdateDto
     {
         [Required(ErrorMessage = "Ingrese el titulo de una pelicula")]
         [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
         [Required(ErrorMessage = "Ingrese una descripción")]
-        [MaxLength (600)]
+        [MaxLength(600)]
         public string? Description { get; set; }
 
     }
