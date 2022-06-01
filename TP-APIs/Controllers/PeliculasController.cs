3af57d6e8497010f7f43bccde63b04e575f1deda
juan-ds <juanods1@gmail.com>
2022-06-01 16:11:07 -0300

    Add PUT & DELETE methods

--- TP-APIs/Controllers/PeliculasController.cs
+++ TP-APIs/Controllers/PeliculasController.cs
@@ -34,10 +34,37 @@ namespace TP_APIs.Controllers
                 Id = ++lastId,
                 Name = pelicula.Name,
                 Description = pelicula.Description,
+
             };
             peliculas.Add(nuevaPelicula);
 
             return CreatedAtRoute("GetPelicula", nuevaPelicula);
         }
+
+        [HttpPut("{id:int}")]
+        public ActionResult UpdatePelicula(int id, PeliculaUpdateDto pelicula)
+        {
+            var peliculaLocationDB = PeliculasData.InstanciaActual.Peliculas.FirstOrDefault(x => x.Id == id);
+            if (peliculaLocationDB == null) return NotFound();
+
+            peliculaLocationDB.Name = pelicula.Name;
+            peliculaLocationDB.Description = pelicula.Description;
+            peliculaLocationDB.Valoracion = new List<ValoracionDto>();
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult DeletePelicula(int id)
+        {
+            var peliculas = PeliculasData.InstanciaActual.Peliculas;
+            var peliculaDelete = PeliculasData.InstanciaActual.Peliculas.FirstOrDefault(x => x.Id == id);
+            if(peliculaDelete == null) return NotFound();
+            peliculas.Remove(peliculas.Where(x => x.Id == id).First());
+
+            return NoContent();
+        }
+
     }
 }
